88e3128544e205f6b8e7cecd8c30924877b9cfad
bigwheel <k.bigwheel+eng@gmail.com>
2013-05-20 01:08:44 +0900

    試行錯誤中

--- src/main/scala/com/herokuapp/mtgbase/realtimedraftanalyzer/FileChangeSimulator.scala
+++ src/main/scala/com/herokuapp/mtgbase/realtimedraftanalyzer/FileChangeSimulator.scala
@@ -5,19 +5,25 @@ import java.io.{PrintWriter, File}
 
 object FileChangeSimulator {
   def main(args: Array[String]) {
-    val filename = "src/test/test-target.txt"
-    val testTarget = new File(filename)
-    if (testTarget.exists())
-      testTarget.delete()
-    val out = new PrintWriter(filename)
+    val filename = "src/test/resources/test-target.txt"
+    deleteFileIfExist(filename)
 
     val samplePickScore = Source.fromFile("src/test/resources/sample-pick-score.txt")
     for (line <- samplePickScore.getLines()) {
+      val out = new PrintWriter(filename)
       println(line)
       out.println(line)
+      out.close()
+      
       if (line.matches("\\s*"))
         Thread.sleep(1000)
     }
-    out.close()
+    deleteFileIfExist(filename)
+  }
+
+  def deleteFileIfExist(filename: String) {
+    val testTarget = new File(filename)
+    if (testTarget.exists())
+      testTarget.delete()
   }
 }
