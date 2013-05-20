fb1027b3d3d26861fa28dd042738c5764ab00c49
bigwheel <k.bigwheel+eng@gmail.com>
2013-05-21 00:55:52 +0900

    色々更新だがパターンマッチによる処理を行う前に一度コミット

--- src/main/scala/com/herokuapp/mtgbase/realtimedraftanalyzer/App.scala
+++ src/main/scala/com/herokuapp/mtgbase/realtimedraftanalyzer/App.scala
@@ -1,6 +1,7 @@
 package com.herokuapp.mtgbase.realtimedraftanalyzer
 
 import java.nio.file._
+import scala.collection.JavaConversions._
 
 object App {
   def main(args: Array[String]) {
@@ -13,5 +14,24 @@ object App {
     val key = dir.register(watcher, StandardWatchEventKinds.ENTRY_CREATE,
       StandardWatchEventKinds.ENTRY_DELETE, StandardWatchEventKinds.ENTRY_MODIFY)
     keys += (key -> dir)
+
+    while(true) {
+      val key = watcher.take
+      val dir = keys.get(key).asInstanceOf[Path]
+
+      for (tmp <- key.pollEvents().toList) {
+        val event = tmp.asInstanceOf[WatchEvent[Path]]
+        val kind = event.kind()
+        if (kind != StandardWatchEventKinds.OVERFLOW) {
+          val ev = kind.asInstanceOf[WatchEvent[Path]]
+          val name = ev.context()
+          val child = dir.resolve(name)
+          System.err.println("%s: %s\n".format(event.kind().name(), child))
+        }
+      }
+      if (!key.reset()) {
+        keys.remove(key)
+      }
+    }
   }
 }
