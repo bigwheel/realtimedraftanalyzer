c370869a5548bbac2dc7e0495b0ac0b1490e90bb
bigwheel <k.bigwheel+eng@gmail.com>
2013-05-21 00:04:41 +0900

    少しずつWatchServiceを使用中

--- src/main/scala/com/herokuapp/mtgbase/realtimedraftanalyzer/App.scala
+++ src/main/scala/com/herokuapp/mtgbase/realtimedraftanalyzer/App.scala
@@ -1,18 +1,17 @@
 package com.herokuapp.mtgbase.realtimedraftanalyzer
 
-import java.nio.file.{FileSystems, WatchService}
+import java.nio.file._
 
 object App {
   def main(args: Array[String]) {
     FileChangeSimulator.start
 
-    while(true) {
     val watcher = FileSystems.getDefault.newWatchService
-    val key = watcher.take
+    val keys = collection.mutable.Map[WatchKey, Path]()
+    val dir = Paths.get("src/test/resources/")
 
-    for (event <- key.pollEvents) {
-
-    }
-    }
+    val key = dir.register(watcher, StandardWatchEventKinds.ENTRY_CREATE,
+      StandardWatchEventKinds.ENTRY_DELETE, StandardWatchEventKinds.ENTRY_MODIFY)
+    keys += (key -> dir)
   }
 }
