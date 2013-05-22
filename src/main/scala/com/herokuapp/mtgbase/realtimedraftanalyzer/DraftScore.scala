361a16da4083f648ca583246ab5b8460313ed131
bigwheel <k.bigwheel+eng@gmail.com>
2013-05-23 01:07:47 +0900

    とりあえず１行目のイベント番号は取り出せた

--- src/main/scala/com/herokuapp/mtgbase/realtimedraftanalyzer/DraftScore.scala
+++ src/main/scala/com/herokuapp/mtgbase/realtimedraftanalyzer/DraftScore.scala
@@ -4,4 +4,8 @@ import scala.io.Source
 
 class DraftScore(path: String) {
   val text = Source.fromFile(path).mkString
+  val it = Source.fromFile(path).getLines
+  val FirstLine = """Event #: (\d+)""".r
+  val FirstLine(eventNumber) = it.next
+  println(eventNumber)
 }
