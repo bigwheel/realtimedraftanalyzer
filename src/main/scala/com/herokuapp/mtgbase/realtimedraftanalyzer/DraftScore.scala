a89aba689aab6cb6024f5cd86c04e64772890894
bigwheel <k.bigwheel+eng@gmail.com>
2013-05-24 07:59:41 +0900

    とりあえず日付・参加者・イベント番号までパース+テストも書いた

--- src/main/scala/com/herokuapp/mtgbase/realtimedraftanalyzer/DraftScore.scala
+++ src/main/scala/com/herokuapp/mtgbase/realtimedraftanalyzer/DraftScore.scala
@@ -5,15 +5,31 @@ import java.text.SimpleDateFormat
 import java.util.Locale
 
 class DraftScore(path: String) {
+  private[this] def throwIAE(line: String = "") = {
+    throw new IllegalArgumentException("入力されたテキストファイルのフォーマットが不正です:" + line)
+  }
+
   private[this] val it = Source.fromFile(path).getLines // itってミュータブルなんやね
 
-  private[this] val eventNumber = """Event #: (\d+)""".r.
+  val eventNumber = """Event #: (\d+)""".r.
       findFirstMatchIn(it.next).get.group(1).toInt
-  private[this] val date = new SimpleDateFormat(
+  val date = new SimpleDateFormat(
     "'Time:    'MM/dd/yyyy hh:mm:ss a", Locale.ENGLISH).parse(it.next)
 
-  if (it.next == "Players: ") {
 
+  val players = if (it.next == "Players: ") {
+    def parsePlayers(it: Iterator[String]): Players = {
+      val recording_player = "^--> (.+)$".r
+      val other_player = "^    (.+)$".r
+      it.next match {
+        case recording_player(name) => Players(name, parsePlayers(it).others)
+        case other_player(name) => parsePlayers(it).appendOtherPlayer(name)
+        case " " => Players()
+        case other => throwIAE(other)
+      }
+    }
+    parsePlayers(it)
   } else
-    throw new IllegalArgumentException("入力されたテキストファイルのフォーマットが不正です")
+    throwIAE()
+
 }
---
+++ src/main/scala/com/herokuapp/mtgbase/realtimedraftanalyzer/Players.scala
@@ -0,0 +1,7 @@
+package com.herokuapp.mtgbase.realtimedraftanalyzer
+
+case class Players(recordingPlayer: String = null, others: List[String] = List.empty[String]) {
+  def appendOtherPlayer(appendingPlayer: String): Players = {
+    Players(this.recordingPlayer, appendingPlayer::this.others)
+  }
+}
