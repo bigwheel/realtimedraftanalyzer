f8344f59e45175c3794b3d2498674d19b1bd3aad
bigwheel <k.bigwheel+eng@gmail.com>
2013-05-25 01:22:43 +0900

    テストの構造を改良して構造化

--- src/test/scala/com/herokuapp/mtgbase/realtimedraftanalyzer/DraftScoreSpec.scala
+++ src/test/scala/com/herokuapp/mtgbase/realtimedraftanalyzer/DraftScoreSpec.scala
@@ -1,7 +1,7 @@
 package com.herokuapp.mtgbase.realtimedraftanalyzer
 
 import org.specs2.mutable.Specification
-import java.util.{Calendar, Date}
+import java.util.Calendar
 
 class DraftScoreSpec extends Specification {
   val sampleFilePath = "src/test/resources/sample-pick-score.txt"
@@ -11,34 +11,36 @@ class DraftScoreSpec extends Specification {
     "be able to be created" in {
       obj must not be throwA[Exception]
     }
-  }
 
-  "DraftScore#eventNumber" should {
-    "be match with that in source file" in {
-      obj.eventNumber must_== 4890335
+    "#eventNumber" in {
+      "be match with that in source file" in {
+        obj.eventNumber must_== 4890335
+      }
     }
-  }
 
-  "DraftScore#date" should {
-    "be match with that in source file" in {
-      val cal = Calendar.getInstance()
-      cal.set(2013, 0, 12, 16, 43, 12)
-      cal.set(Calendar.MILLISECOND, 0)
-      //obj.date.compareTo(cal.getTime) must_== 0
-      obj.date must_== cal.getTime
+    "#date" in {
+      "be match with that in source file" in {
+        val cal = Calendar.getInstance()
+        cal.set(2013, 0, 12, 16, 43, 12)
+        cal.set(Calendar.MILLISECOND, 0)
+        //obj.date.compareTo(cal.getTime) must_== 0
+        obj.date must_== cal.getTime
+      }
     }
-  }
 
-  "DraftScore#players" should {
-    "be match with that in source file" in {
-      obj.players must_== Players("bigwheel", List("sheepmage", "physique",
-        "jwj8717", "jpfeinberg", "td_tsunehito", "lackystar", "motoo"))
+    "#players" in {
+      "be match with that in source file" in {
+        obj.players must_== Players("bigwheel", List("sheepmage", "physique",
+          "jwj8717", "jpfeinberg", "td_tsunehito", "lackystar", "motoo"))
+      }
     }
-  }
 
-  "DraftScore#firstPack#expansion" should {
-    "be match with that in source file" in {
-      obj.firstPack.expansion must_== "RTR"
+    "#firstPack" in {
+      "#expansion" in {
+        "be match with that in source file" in {
+          obj.firstPack.expansion must_== "RTR"
+        }
+      }
     }
   }
 
