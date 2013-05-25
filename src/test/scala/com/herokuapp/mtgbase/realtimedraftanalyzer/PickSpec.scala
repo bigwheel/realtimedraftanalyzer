f674b2e7c46a7cdc87376702b1639d87986cce5e
bigwheel <k.bigwheel+eng@gmail.com>
2013-05-25 18:46:56 +0900

    テストケースを追加

--- src/test/scala/com/herokuapp/mtgbase/realtimedraftanalyzer/PickSpec.scala
+++ src/test/scala/com/herokuapp/mtgbase/realtimedraftanalyzer/PickSpec.scala
@@ -94,7 +94,33 @@ class PicksOfAPackSpec extends Specification {
 
     "if pickNumbers are not contiguous" in {
       PicksOfAPack("適当なエキスパンション名", List(
-        Pick(1, 1, dummyCardList(15)), /*Pick(2, 2, dummyCardList(14)),*/
+        Pick(1, 1, dummyCardList(15)), /*Pick(1, 2, dummyCardList(14)),*/
+        Pick(1, 3, dummyCardList(13)), Pick(1, 4, dummyCardList(12)),
+        Pick(1, 5, dummyCardList(11)), Pick(1, 6, dummyCardList(10)),
+        Pick(1, 7, dummyCardList(9)), Pick(1, 8, dummyCardList(8)),
+        Pick(1, 9, dummyCardList(7)), Pick(1, 10, dummyCardList(6)),
+        Pick(1, 11, dummyCardList(5)), Pick(1, 12, dummyCardList(4)),
+        Pick(1, 13, dummyCardList(3)), Pick(1, 14, dummyCardList(2)),
+        Pick(1, 15, dummyCardList(1))
+      )) must throwAn[Exception]
+    }
+
+    "if pickNumbers are not contiguous" in {
+      PicksOfAPack("適当なエキスパンション名", List(
+        Pick(1, 1, dummyCardList(15)), Pick(1, 2, dummyCardList(14)),
+        Pick(1, 3, dummyCardList(13)), Pick(1, 4, dummyCardList(12)),
+        Pick(1, 5, dummyCardList(11)), Pick(1, 6, dummyCardList(10)),
+        Pick(1, 7, dummyCardList(9)), Pick(1, 8, dummyCardList(8)),
+        Pick(1, 9, dummyCardList(7)), Pick(1, 10, dummyCardList(6)),
+        Pick(1, 11, dummyCardList(5)), Pick(1, 12, dummyCardList(4)),
+        Pick(1, 13, dummyCardList(3)), Pick(1, 14, dummyCardList(2))/*,
+        Pick(1, 15, dummyCardList(1))*/
+      )) must throwAn[Exception]
+    }
+
+    "if pickNumbers are not contiguous" in {
+      PicksOfAPack("適当なエキスパンション名", List(
+        /*Pick(1, 1, dummyCardList(15)),*/ Pick(1, 2, dummyCardList(14)),
         Pick(1, 3, dummyCardList(13)), Pick(1, 4, dummyCardList(12)),
         Pick(1, 5, dummyCardList(11)), Pick(1, 6, dummyCardList(10)),
         Pick(1, 7, dummyCardList(9)), Pick(1, 8, dummyCardList(8)),
