9f7fc775bd3152b421bed41c36069332ee06b02e
bigwheel <k.bigwheel+eng@gmail.com>
2013-05-26 18:08:45 +0900

    HashMapをscala組み込みのMapへ変換した

--- src/main/scala/com/herokuapp/mtgbase/realtimedraftanalyzer/DirectoryWatcher.scala
+++ src/main/scala/com/herokuapp/mtgbase/realtimedraftanalyzer/DirectoryWatcher.scala
@@ -1,24 +1,25 @@
 package com.herokuapp.mtgbase.realtimedraftanalyzer
 
 import java.nio.file._
-import java.util.HashMap
 import scala.collection.JavaConversions._
 
 class DirectoryWatcher(directoryPath: String,
                        eventHandler: ((WatchEvent[Path], Path) => Unit)) {
   // constructor
   val watcher = FileSystems.getDefault.newWatchService()
-  val keys = new HashMap[WatchKey, Path]()
   val dir = Paths.get(directoryPath)
 
   // register
   val key = dir.register(watcher, StandardWatchEventKinds.ENTRY_CREATE,
     StandardWatchEventKinds.ENTRY_DELETE, StandardWatchEventKinds.ENTRY_MODIFY)
-  keys.put(key, dir)
+  val keys: Map[WatchKey, Path] = Map(key -> dir)
 
   while(true) {
     val key = watcher.take()
-    val dir = keys.get(key)
+    val dir = keys.get(key) match {
+      case Some(n) => n
+      case _ => throw new IllegalStateException("keysの状態またはkeyの値が不正です")
+    }
 
     for (tmp <- key.pollEvents().toList) {
       val event = tmp.asInstanceOf[WatchEvent[Path]]
@@ -29,8 +30,7 @@ class DirectoryWatcher(directoryPath: String,
         eventHandler(event, fullpath)
       }
     }
-    if (!key.reset()) {
+    if (!key.reset())
       keys.remove(key)
-    }
   }
 }
