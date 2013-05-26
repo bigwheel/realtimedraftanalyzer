a12fd7a4a69079fc5ca46357a86dcc6eedb762b6
bigwheel <k.bigwheel+eng@gmail.com>
2013-05-26 19:28:03 +0900

    ThreadからActorへ並列処理を切り替えた

--- src/main/scala/com/herokuapp/mtgbase/realtimedraftanalyzer/FileChangeSimulator.scala
+++ src/main/scala/com/herokuapp/mtgbase/realtimedraftanalyzer/FileChangeSimulator.scala
@@ -2,9 +2,10 @@ package com.herokuapp.mtgbase.realtimedraftanalyzer
 
 import io.Source
 import java.io.{PrintWriter, File}
+import scala.actors.Actor
 
-class FileChangeSimulator(source: String, dest: String) extends Thread {
-  override def run() {
+class FileChangeSimulator(source: String, dest: String) {
+  Actor.actor {
     deleteFileIfExist(source)
 
     val samplePickScore = Source.fromFile(dest)
