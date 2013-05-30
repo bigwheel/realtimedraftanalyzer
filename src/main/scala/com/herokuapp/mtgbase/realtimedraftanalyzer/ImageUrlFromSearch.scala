8b376200324720ee81b18640136102e432c1bf89
bigwheel <k.bigwheel+eng@gmail.com>
2013-05-30 23:46:59 +0900

    とりあえずテストを満たさない最低限実装をした

--- src/main/scala/com/herokuapp/mtgbase/realtimedraftanalyzer/ImageUrlFromSearch.scala
+++ src/main/scala/com/herokuapp/mtgbase/realtimedraftanalyzer/ImageUrlFromSearch.scala
@@ -1,4 +1,7 @@
 package com.herokuapp.mtgbase.realtimedraftanalyzer
 
 object ImageUrlFromSearch {
+  def apply(cardName: String): Option[String] = {
+    Some("http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=253536&type=card")
+  }
 }
--- src/test/scala/com/herokuapp/mtgbase/realtimedraftanalyzer/ImageUrlFromSearchSpec.scala
+++ src/test/scala/com/herokuapp/mtgbase/realtimedraftanalyzer/ImageUrlFromSearchSpec.scala
@@ -1,11 +1,21 @@
 package com.herokuapp.mtgbase.realtimedraftanalyzer
 
 import org.specs2.mutable.Specification
+import org.specs2.specification.AllExpectations
 
-class ImageUrlFromSearchSpec extends Specification {
+class ImageUrlFromSearchSpec extends Specification with AllExpectations {
   "ImageUrlFromSearch" should {
+    def url(id: String) =
+      "http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=" + id + "&type=card"
+    val validPairs = List(("Horncaller's Chant", url("253536")),
+      ("Goblin Electromancer", url("338414")),
+      ("Nivmagus Elemental", url("290526")),
+      ("Æther Adept", url("227222"))
+    )
+
     "return valid Image Url When valid name given" in {
-      pending
+      for (validPair <- validPairs)
+        ImageUrlFromSearch(validPair._1) must_== Some(validPair._2)
     }
 
     "return None When invalid name given" in {
