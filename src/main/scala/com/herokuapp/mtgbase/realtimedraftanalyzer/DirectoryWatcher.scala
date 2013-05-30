38d0598246e295776a0f2a50631c5a10c12fc5cb
bigwheel <k.bigwheel+eng@gmail.com>
2013-05-31 01:29:15 +0900

    DirectoryWatcherを並列処理のためループ処理をactor化

--- src/main/scala/com/herokuapp/mtgbase/realtimedraftanalyzer/DirectoryWatcher.scala
+++ src/main/scala/com/herokuapp/mtgbase/realtimedraftanalyzer/DirectoryWatcher.scala
@@ -3,6 +3,7 @@ package com.herokuapp.mtgbase.realtimedraftanalyzer
 import java.nio.file._
 import scala.collection.JavaConversions._
 import scala.util.control.Breaks
+import scala.actors.Actor.actor
 
 class DirectoryWatcher(directoryPath: String,
                        eventHandler: ((WatchEvent[Path], Path) => Boolean)) {
@@ -15,27 +16,29 @@ class DirectoryWatcher(directoryPath: String,
     StandardWatchEventKinds.ENTRY_DELETE, StandardWatchEventKinds.ENTRY_MODIFY)
   val keys: Map[WatchKey, Path] = Map(key -> dir)
 
-  Breaks.breakable {
-    while(true) {
-      val key = watcher.take()
-      val dir = keys.get(key) match {
-        case Some(n) => n
-        case _ => throw new IllegalStateException("keysの状態またはkeyの値が不正です")
-      }
-
-      for (tmp <- key.pollEvents().toList) {
-        val event = tmp.asInstanceOf[WatchEvent[Path]]
-        val kind = event.kind()
-        if (kind != StandardWatchEventKinds.OVERFLOW) {
-          val filename = event.context()
-          val fullpath = dir.resolve(filename)
-          if (eventHandler(event, fullpath) == false)
-            Breaks.break
-
+  actor {
+    Breaks.breakable {
+      while(true) {
+        val key = watcher.take()
+        val dir = keys.get(key) match {
+          case Some(n) => n
+          case _ => throw new IllegalStateException("keysの状態またはkeyの値が不正です")
         }
+
+        for (tmp <- key.pollEvents().toList) {
+          val event = tmp.asInstanceOf[WatchEvent[Path]]
+          val kind = event.kind()
+          if (kind != StandardWatchEventKinds.OVERFLOW) {
+            val filename = event.context()
+            val fullpath = dir.resolve(filename)
+            if (eventHandler(event, fullpath) == false)
+              Breaks.break
+
+          }
+        }
+        if (!key.reset())
+          keys.remove(key)
       }
-      if (!key.reset())
-        keys.remove(key)
     }
   }
 }
