840873c11b51f3d3bf7fbcf0584ece797656247a
bigwheel <k.bigwheel+eng@gmail.com>
2013-05-31 00:16:12 +0900

    URLは意図通り取れるようになったが
     * エキスパンションの違いで過去のエキスパンションURLをとってしまい
     テストが失敗する

--- src/main/scala/com/herokuapp/mtgbase/realtimedraftanalyzer/ImageUrlFromSearch.scala
+++ src/main/scala/com/herokuapp/mtgbase/realtimedraftanalyzer/ImageUrlFromSearch.scala
@@ -1,7 +1,18 @@
 package com.herokuapp.mtgbase.realtimedraftanalyzer
 
+import scala.io.Source
+import java.net.URLEncoder
+import scala.util.parsing.json.JSON
+
 object ImageUrlFromSearch {
   def apply(cardName: String): Option[String] = {
-    Some("http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=253536&type=card")
+    val encordedCardName = URLEncoder.encode(cardName, "UTF-8")
+    val resultString = Source.fromURL(
+      "http://mtgbase.herokuapp.com/search?card_name=" + encordedCardName).
+      getLines.mkString
+    val resultJson = JSON.parseFull(resultString)
+    val map = resultJson.get.asInstanceOf[List[Any]].head.
+      asInstanceOf[Map[String, Option[Any]]]
+    Some(map.get("card_image_url").get.asInstanceOf[String])
   }
 }
