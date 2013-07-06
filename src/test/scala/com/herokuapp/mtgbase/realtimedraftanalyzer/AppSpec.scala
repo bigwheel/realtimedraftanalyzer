fad164b9a00778a930ff1a64eee2a2cc63703d69
k.bigwheel <k.bigwheel+github@gmail.com>
2013-07-07 02:51:15 +0900

    AppのテストをSpecの方に写した
     * しかしきちんとしたテストはSwingを大勝にしたライブラリなどを用いてきちんと書く必要あり

--- src/test/scala/com/herokuapp/mtgbase/realtimedraftanalyzer/AppSpec.scala
+++ src/test/scala/com/herokuapp/mtgbase/realtimedraftanalyzer/AppSpec.scala
@@ -3,16 +3,19 @@ package com.herokuapp.mtgbase.realtimedraftanalyzer
 import org.specs2.mutable._
 
 class AppSpec extends Specification {
+  "App" should {
+    "not throw exception in processing sample pick score" in {
+      // テストでおいてるだけなのできちんと削除すること
+      new FileChangeSimulator("src/test/resources/test-target.txt",
+        "src/test/resources/sample-pick-score.txt", 10000)
 
-  "The 'Hello world' string" should {
-    "contain 11 characters" in {
-      "Hello world" must have size(11)
-    }
-    "start with 'Hello'" in {
-      "Hello world" must startWith("Hello")
-    }
-    "end with 'world'" in {
-      "Hello world" must endWith("world")
+      var a = false
+      App.reactions += {
+        case _: swing.event.WindowClosed => a = true
+      }
+      App.main(Array.empty[String])
+      while (a == false)
+        Thread.sleep(1000) must not throwA
     }
   }
 }
